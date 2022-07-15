9d321da706dcd9a1769364c044b9eb96fa126418
WilhelmYakunin <v.jakunin@inbox.ru>
2022-07-15 09:34:23 +0300

    refactor gendiff methods to sream

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import { resolve } from 'path';
 import { readFileSync } from 'fs';
 import genDiff from './gendiff.js';
 
-
 const getFixturePath = (filename) => resolve('../', '__fixtures__', filename);
 const read = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const fixturesFormates = ['json', 'ini', 'yml'];
 const styles = ['json', 'plain', 'stylish'];
 
-styles.forEach((style) => {
-  const fileBefore = getFixturePath(`before.${'json'}`);
-  const fileAfter = getFixturePath(`after.${'json'}`);
-  const tested = genDiff(fileBefore, fileAfter, style);
-  const expected = read(`result-${style}.txt`);
-  console.log(tested, '////', expected);
+describe.each(fixturesFormates)('%s', (format) => {
+  test(`${format} genDiff test`, () => {
+    styles.forEach((style) => {
+      const fileBefore = getFixturePath(`before.${format}`);
+      const fileAfter = getFixturePath(`after.${format}`);
+      const tested = genDiff(fileBefore, fileAfter, style);
+      const expected = read(`result-${style}.txt`);
+      expect(tested).toEqual(expected);
+    });
+  });
 });
